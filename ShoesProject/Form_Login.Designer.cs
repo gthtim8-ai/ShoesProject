a03889ce5c63a674feb30b3e688f8476ec9a95a7
KB <kbolyarskih@yandeex.ru>
2026-06-05 14:11:05 +0500

    Создание формы Form_Login

---
+++ ShoesProject/Form_Login.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace ShoesProject
+{
+    partial class Form_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Login));
+            pblogo = new PictureBox();
+            pnMain = new Panel();
+            btnGuest = new Button();
+            btnLogin = new Button();
+            txtPassword = new TextBox();
+            lbPassword = new Label();
+            txtLogin = new TextBox();
+            lbLogin = new Label();
+            ((System.ComponentModel.ISupportInitialize)pblogo).BeginInit();
+            pnMain.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pblogo
+            // 
+            pblogo.Anchor = AnchorStyles.None;
+            pblogo.Image = (Image)resources.GetObject("pblogo.Image");
+            pblogo.Location = new Point(142, 21);
+            pblogo.Name = "pblogo";
+            pblogo.Size = new Size(100, 100);
+            pblogo.SizeMode = PictureBoxSizeMode.Zoom;
+            pblogo.TabIndex = 0;
+            pblogo.TabStop = false;
+            // 
+            // pnMain
+            // 
+            pnMain.Controls.Add(btnGuest);
+            pnMain.Controls.Add(btnLogin);
+            pnMain.Controls.Add(txtPassword);
+            pnMain.Controls.Add(lbPassword);
+            pnMain.Controls.Add(txtLogin);
+            pnMain.Controls.Add(lbLogin);
+            pnMain.Location = new Point(12, 127);
+            pnMain.Name = "pnMain";
+            pnMain.Size = new Size(360, 210);
+            pnMain.TabIndex = 1;
+            pnMain.Paint += panel1_Paint;
+            // 
+            // btnGuest
+            // 
+            btnGuest.BackColor = Color.Chartreuse;
+            btnGuest.FlatAppearance.BorderSize = 0;
+            btnGuest.FlatStyle = FlatStyle.Flat;
+            btnGuest.Location = new Point(105, 163);
+            btnGuest.Name = "btnGuest";
+            btnGuest.Size = new Size(150, 30);
+            btnGuest.TabIndex = 5;
+            btnGuest.Text = "Войти как гость";
+            btnGuest.UseVisualStyleBackColor = false;
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = Color.MediumSpringGreen;
+            btnLogin.FlatAppearance.BorderSize = 0;
+            btnLogin.FlatStyle = FlatStyle.Flat;
+            btnLogin.Location = new Point(105, 128);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(150, 30);
+            btnLogin.TabIndex = 4;
+            btnLogin.Text = "Войти";
+            btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(55, 97);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(250, 26);
+            txtPassword.TabIndex = 3;
+            txtPassword.UseSystemPasswordChar = true;
+            // 
+            // lbPassword
+            // 
+            lbPassword.AutoSize = true;
+            lbPassword.Location = new Point(151, 73);
+            lbPassword.Name = "lbPassword";
+            lbPassword.Size = new Size(58, 19);
+            lbPassword.TabIndex = 2;
+            lbPassword.Text = "Пароль";
+            // 
+            // txtLogin
+            // 
+            txtLogin.Location = new Point(55, 42);
+            txtLogin.Name = "txtLogin";
+            txtLogin.Size = new Size(250, 26);
+            txtLogin.TabIndex = 1;
+            // 
+            // lbLogin
+            // 
+            lbLogin.Anchor = AnchorStyles.None;
+            lbLogin.AutoSize = true;
+            lbLogin.Location = new Point(154, 18);
+            lbLogin.Name = "lbLogin";
+            lbLogin.Size = new Size(52, 19);
+            lbLogin.TabIndex = 0;
+            lbLogin.Text = "Логин";
+            // 
+            // Form_Login
+            // 
+            AutoScaleDimensions = new SizeF(9F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(384, 361);
+            Controls.Add(pnMain);
+            Controls.Add(pblogo);
+            Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Margin = new Padding(4);
+            MaximizeBox = false;
+            Name = "Form_Login";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Вход в систему";
+            ((System.ComponentModel.ISupportInitialize)pblogo).EndInit();
+            pnMain.ResumeLayout(false);
+            pnMain.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pblogo;
+        private Panel pnMain;
+        private Button btnGuest;
+        private Button btnLogin;
+        private TextBox txtPassword;
+        private Label lbPassword;
+        private TextBox txtLogin;
+        private Label lbLogin;
+    }
+}
